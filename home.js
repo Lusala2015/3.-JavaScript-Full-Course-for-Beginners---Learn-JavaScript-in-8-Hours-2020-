ebe0548bdc3a1618ebd6662e39a186d18adc28a5
Lusala2015 <lusalamark17@gmail.com>
2020-07-13 12:24:05 +0300

    end

--- home.js
+++ home.js
@@ -9,4 +9,8 @@ console.log(b);
 var someNumber = 45;
 console.log(someNumber);
 
-document.getElementById('someText').innerHTML = "mark lusala";
+//document.getElementById('someText').innerHTML = "mark lusala";
+
+var age = prompt('What is your age?'); 
+
+document.getElementById('someText').innerHTML = age;
