4f72943f4ec1a396f2b75a759038a724ab7e373e
Lusala2015 <lusalamark17@gmail.com>
2020-07-13 12:19:42 +0300

    js changes the information on a html file using the getElemenyById() function.

--- home.js
+++ home.js
@@ -2,3 +2,11 @@ console.log("hello");
 //alert("yoooo"); ===> How to write a comment inline.
 
 //variables 
+var b = 'Mark';
+
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById('someText').innerHTML = "mark lusala";
